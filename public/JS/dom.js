38d72a24b2b8fc3a40acf09c09459341a1a4cf31
EngAyoosh <aaa_2008azhar@hotmail.com>
2017-08-09 17:52:23 +0300

    edit index.html and dom

--- public/JS/dom.js
+++ public/JS/dom.js
@@ -2,26 +2,23 @@
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 
-(function() {
+( function () {
     // This is the dom node where we will keep our todo
-    var container = document.getElementById('flags');
-    var addTodoForm = document.getElementById('ulContainer');
-    var country = document.querySelectorAll('.country')
+  var country = document.querySelectorAll('.country')
 
-
-    country.forEach(function(element) {
-        element.addEventListener('click', function() {
-            request("\search?c=" + element.getAttribute('number'), function(res) {
+    country.forEach(function (element) {
+        element.addEventListener('click', function () {
+            request('\search?c=' + element.getAttribute('number'), function (res) {
                 fill(res)
             })
-
-
-        });
+        })
     })
-    function fill(arr) {
-        for (let i = 1; i <= 10; i++) {
-           document.querySelector('#item' + i).textContent=arr[i-1]
+    function fill (arr) {
+      for (let i = 1; i <= 10; i++) {
+        document.querySelector('#a' + i).href = 'http://google.com/search?q=' + arr[i - 1]
+        document.querySelector('#a' + i).target = '_blank'
+        document.querySelector('#item' + i).textContent = arr[ i - 1 ]
         }
-    }
 
-})();
+}
+})()
